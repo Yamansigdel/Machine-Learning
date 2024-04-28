f741eb0950416c4fedb2787bd48551f8ecab9ed0
Yaman Sigdel <Yamansigdel999@gmail.com>
2024-04-28 17:50:29 +0545

    commit

--- Supervised learning/simple_linear_regression.py
+++ Supervised learning/simple_linear_regression.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-#plt.style.use('./deeplearning.mplstyle')
+plt.style.use('./deeplearning.mplstyle')
 
 x_train=np.array([1.0,2.0])
 y_train=np.array([300.0,500.0])
