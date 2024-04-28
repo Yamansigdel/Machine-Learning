a5d524046c9694d65d60fb29757542323b91c1f2
Yaman Sigdel <Yamansigdel999@gmail.com>
2024-04-28 17:51:57 +0545

    .

--- Supervised learning/cost_function for one variable.py
+++ Supervised learning/cost_function for one variable.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
-#plt.style.use('./deeplearning.mplstyle')
+plt.style.use('./deeplearning.mplstyle')
 
 x_train = np.array([1.0, 2.0])           #(size in 1000 square feet)
 y_train = np.array([300.0, 500.0])           #(price in 1000s of dollars)
